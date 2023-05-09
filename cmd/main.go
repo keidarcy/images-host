64ae8b0d913249c3daefb6f6ccfa8c4b68ff61a3
Xing Yahao <keidarcy.1015@gmail.com>
2023-05-10 08:08:20 +0900

    feat: add ci build

---
+++ cmd/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"os"
+
+	"github.com/keidarcy/zzw-food-gallery/pkg/util"
+)
+
+func main() {
+	ci := false
+	if len(os.Args) > 1 {
+		ci = true
+	}
+
+	if !ci {
+		// local
+		// move files from ~/Downloads/x.HEIC to ./heic-images
+		util.Move()
+		// get heic
+		// convert to jpg
+		util.Convert()
+		// upload jpg to s3
+		util.Upload()
+		// remove heic and jpg
+		util.Clean()
+	}
+	// get from s3
+	// render html
+	util.Render()
+}
